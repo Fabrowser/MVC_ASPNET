3bcadfa1bee8a6f3352d4bb25fdfdb0f6ca353d4
Fabrowser <fabriciocarlosguima@gmail.com>
2023-09-10 15:00:40 -0300

    Anterior ao capitulo Bootstrap - Cap 01 e 02

---
+++ MVC_Capitulo01/Controllers/InstituicaoController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.EntityFrameworkCore;
+using MVC_Capitulo01.Data;
+using MVC_Capitulo01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Capitulo01.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+
+        private readonly IESContext _context;
+
+        public InstituicaoController(IESContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(_context.Instituicoes.OrderBy(i => i.Nome));
+        }
+
+        // GET: Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Instituicao instituicao)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(instituicao);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Não foi possível inserir novos dados!");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instituicao = await _context.Instituicoes.FindAsync(id);
+
+
+            if (instituicao == null)
+            {
+                return NotFound();
+            }
+
+            return View(instituicao);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Instituicao instituicao)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(instituicao);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InstituicaoExists(instituicao.InstituicaoID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instituicao);
+        }
+
+        private bool InstituicaoExists(long? id)
+        {
+            return _context.Instituicoes.Any(e => e.InstituicaoID == id
+            );
+        }
+
+
+        public async Task<IActionResult> Details(long? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+
+            var instituicao = await _context.Instituicoes.SingleOrDefaultAsync(i => i.InstituicaoID == id);
+            if (instituicao == null)
+            {
+                return NotFound();
+            }
+            return View(instituicao);
+        }
+
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var instituicao = await _context.Instituicoes.SingleOrDefaultAsync(m => m.InstituicaoID == id);
+            if (instituicao == null)
+            {
+                return NotFound();
+            }
+            return View(instituicao);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long? id)
+        {
+
+            var instituicao = await _context.Instituicoes.SingleOrDefaultAsync(i => i.InstituicaoID == id);
+
+            _context.Instituicoes.Remove(instituicao);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ MVC_Capitulo01/Data/IESContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Capitulo01.Data;
+using MVC_Capitulo01.Models;
+
+
+namespace MVC_Capitulo01.Data
+{
+
+    public class IESContext:  DbContext
+    {
+
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Departamento>().ToTable("Departamentos");
+        }
+
+    }
+
+
+}
